fd4a9ee72d9fabdb679b7d63c6d5fd0ba21f9c40
Diefferson Santos <diefferson.sts@gmail.com>
2017-03-28 19:30:43 -0300

    atualizaçao arquivos

--- app/controllers.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Home
-$container['HomeController']  = function($container) use ($app){
-	return new Controllers\HomeController($container);
-};
--- app/routes.php
+++
@@ -1,3 +0,0 @@
-<?php
-	
-$app->get('/homepet/', 'HomeController:index')->setName('homepage');
